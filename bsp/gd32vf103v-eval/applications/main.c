8ea6b56f4a4712dbf5d22989a0b9f2bc26ca0eaa
iysheng <iysheng@163.com>
2021-04-07 01:07:53 +0800

    [CHG][TEST] 基本功能测试通过

--- bsp/gd32vf103v-eval/applications/main.c
+++ bsp/gd32vf103v-eval/applications/main.c
@@ -18,6 +18,28 @@
 #endif
 
 #define INFRA_SINGAL_PIN     6
+#define INFRA_CONTROL_PIN    7
+
+#define INFRA_RELAY0_PIN     4
+#define INFRA_RELAY1_PIN     2
+
+static int _set_pin_channel(int channel)
+{
+    if (channel > 15)
+    {
+        rt_kprintf("invalid channel");
+        return -E2BIG;
+    }
+    else
+    {
+        rt_kprintf("set channel=%d\n", channel);
+    }
+
+    /* BIT[12:15] */
+    GPIO_OCTL(GPIOB) &= ~(uint32_t)0xf000;
+    GPIO_OCTL(GPIOB) |= (uint32_t)(channel << 12);
+    return 0;
+}
 
 static rt_device_t s_seg_dev_ptr;
 /**
@@ -44,12 +66,6 @@ static void blink_test(void)
     }
     rt_kprintf("hello china. %d\n", led_status++);
     gpio_bit_write(GPIOC, GPIO_PIN_9, led_status % 2);
-#if 0
-    if (s_seg_dev_ptr)
-    {
-        rt_device_write(s_seg_dev_ptr, 0, &led_status, 1);
-    }
-#endif
 }
 
 /*
@@ -64,7 +80,7 @@ static void blink_test(void)
 static void test_infra_pin_level(void)
 {
 	static int s_pin_inited_done;
-	static unsigned char pin_level;
+	static unsigned char pin_level, pin_channel, scan_cycle;
 
     if (!s_pin_inited_done)
     {
@@ -73,17 +89,49 @@ static void test_infra_pin_level(void)
 #else
         rt_pin_mode(INFRA_SINGAL_PIN, PIN_MODE_INPUT);
 #endif
+        /* enable chip select */
+        rt_pin_mode(INFRA_CONTROL_PIN, PIN_MODE_OUTPUT);
+        rt_pin_write(INFRA_CONTROL_PIN, PIN_LOW);
+
+        rt_pin_mode(INFRA_RELAY0_PIN, PIN_MODE_OUTPUT);
+        rt_pin_write(INFRA_RELAY0_PIN, PIN_LOW);
+        rt_pin_mode(INFRA_RELAY1_PIN, PIN_MODE_OUTPUT);
+        rt_pin_write(INFRA_RELAY1_PIN, PIN_LOW);
+        /* set addr pin mode output */
+        gpio_init(GPIOB, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_12);
+        gpio_init(GPIOB, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_13);
+        gpio_init(GPIOB, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_14);
+        gpio_init(GPIOB, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_15);
+        s_pin_inited_done = 1;
     }
 
+	_set_pin_channel(pin_channel);
 #ifdef INFRA_SEND_DEVICE
-	rt_pin_write(INFRA_SINGAL_PIN, 0 % 2);
-	rt_kprintf("set infra signal is:%u\n", pin_level);
+    rt_pin_write(INFRA_CONTROL_PIN, PIN_HIGH);
+	//rt_pin_write(INFRA_SINGAL_PIN, 0);
+	rt_kprintf("set infra signal channel is:%u\n", pin_channel);
 #else
 	pin_level = (unsigned char)rt_pin_read(INFRA_SINGAL_PIN);
 	rt_kprintf("pin_level of infra signal is:%u\n", pin_level);
-
-    rt_device_write(s_seg_dev_ptr, 0, &pin_level, 1);
+	if (pin_level == 1)
+	{
+        rt_device_write(s_seg_dev_ptr, 0, &pin_channel, 1);
+        rt_pin_write(INFRA_RELAY0_PIN, PIN_HIGH);
+        rt_pin_write(INFRA_RELAY1_PIN, PIN_HIGH);
+        scan_cycle = 1;
+	}
 #endif
+    pin_channel++;
+	if (pin_channel == 16)
+	{
+		pin_channel = 0;
+		if (!scan_cycle)
+		{
+			rt_pin_write(INFRA_RELAY0_PIN, PIN_LOW);
+			rt_pin_write(INFRA_RELAY1_PIN, PIN_LOW);
+		}
+        scan_cycle = 0;
+	}
 }
 
 int main(int argc, char *argv[]) {
@@ -93,7 +141,11 @@ int main(int argc, char *argv[]) {
         blink_test();
         test_infra_pin_level();
         /* TODO read pin level */
-        rt_thread_mdelay(500);
+#ifdef INFRA_SEND_DEVICE
+        rt_thread_mdelay(50);
+#else
+        rt_thread_mdelay(5);
+#endif
     }
 
     return RT_EOK;
