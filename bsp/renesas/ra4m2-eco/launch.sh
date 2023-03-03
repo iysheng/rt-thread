bbddc67af5427a38c127434e81828df2fd9bccb3
iysheng <iysheng@163.com>
2023-03-03 19:04:29 +0800

    [CHG][N/T] 更新编译文件

--- bsp/renesas/ra4m2-eco/launch.sh
+++ bsp/renesas/ra4m2-eco/launch.sh
@@ -2,4 +2,4 @@
 
 # 启动 openocd 加载配置文件
 
-openocd -s openocd_scripts -f renesas.cfg -d3 -l /tmp/dbg_openocd_renesas.txt
+openocd -s openocd_scripts -s /home/red/just4github/openocd/install_local/share/openocd/scripts -f renesas.cfg -d3 -l /tmp/dbg_openocd_renesas.txt
